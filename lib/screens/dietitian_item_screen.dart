c9e450954e6cd9d3ddc7fb90b0b684048ca6ba57
nikkirosche <101503635+nikkirosche@users.noreply.github.com>
2022-11-06 02:42:13 +0800

    image-displayed

--- lib/screens/dietitian_item_screen.dart
+++ lib/screens/dietitian_item_screen.dart
@@ -26,22 +26,43 @@ class _DietitianItemScreenState extends State<DietitianItemScreen> {
         return data.id.contains(dietitianId);
       },
     ).toList();
-    // final selectedDietitian =
-    //     DUMMY_DIETITIANS.firstWhere((dietitian) => dietitian.id == dietitianId);
+    final selectedDietitian =
+        DUMMY_DIETITIANS.firstWhere((dietitian) => dietitian.id == dietitianId);
     return Scaffold(
       appBar: AppBar(
         title: Text("Dietitian"),
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return DietitianItem(
-              id: displayDietitian[index].id,
-              name: displayDietitian[index].name,
-              imageurl: displayDietitian[index].imageUrl,
-              experience: displayDietitian[index].experience,
-              background: displayDietitian[index].background);
-        },
-        itemCount: displayDietitian.length,
+      body: SingleChildScrollView(
+        child: Column(children: <Widget>[
+          Container(
+            height: 350,
+            width: double.infinity,
+            child: Image.network(selectedDietitian.imageUrl, fit: BoxFit.cover),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
+            height: 150,
+            width: 300,
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return DietitianItem(
+                  id: selectedDietitian.id,
+                  name: selectedDietitian.name,
+                  imageurl: selectedDietitian.imageUrl,
+                  experience: selectedDietitian.experience,
+                  background: selectedDietitian.background,
+                );
+              },
+              itemCount: displayDietitian.length,
+            ),
+          ),
+        ]),
       ),
     );
   }
